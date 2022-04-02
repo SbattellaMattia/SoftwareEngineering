3d3116af56dd3dc18b6e6f62baf3430da0c358a0
Valerio Morelli <valeriomorelli50@gmail.com>
2022-04-02 20:09:15 +0200

    second commit

--- Software Museum/Examples/MostUsedInList.py
+++
@@ -1,3 +0,0 @@
-# Trovare l'elemento con più ricorrenze
-test = [1, 2, 3, 4, 2, 2, 3, 1, 1, 1, 4]
-print(max(set(test), key=test.count))
--- Software Museum/Examples/ReverserString.py
+++
@@ -1,3 +0,0 @@
-# è una parola palindroma?
-string = input("Gimme a string -->")
-print(string[::-1] == string)
